16d10cecf3574f79a07afa50a20a9967cf3cba0a
Ubpa <641614112@qq.com>
2019-02-09 18:30:58 +0800

    Update GStorage.h

--- include/CppUtil/Basic/GStorage.h
+++ include/CppUtil/Basic/GStorage.h
@@ -33,6 +33,9 @@ namespace CppUtil {
 				static bool Reg(const ID_Type & uniqueID, const T & item) {
 					return _GStorage<ID_Type, T>::GetInstance()->Reg(uniqueID, item);
 				}
+				static T * GetP(const ID_Type & uniqueID) {
+					return _GStorage<ID_Type, T>::GetInstance()->GetP(uniqueID);
+				}
 			};
 
 			template<typename ID_Type, typename T>
@@ -40,6 +43,10 @@ namespace CppUtil {
 				static bool Reg(const std::string & uniqueID, const T & item) {
 					return _GStorage<std::string, T>::GetInstance()->Reg(uniqueID, item);
 				}
+				static T * GetP(const std::string & uniqueID) {
+					return _GStorage<std::string, T>::GetInstance()->GetP(uniqueID);
+				}
+
 			};
 
 		public:
@@ -50,12 +57,12 @@ namespace CppUtil {
 
 			template<typename ID_Type, typename T>
 			static void GetP(const ID_Type & uniqueID, T * & p) {
-				p = _GStorage<ID_Type, T>::GetInstance()->GetP(uniqueID);
+				p = Wrapper<ID_Type, T, std::is_convertible<ID_Type, std::string>::value>::GetP(uniqueID);
 			}
 
 			template<typename ID_Type, typename T>
 			static bool GetV(const ID_Type & uniqueID, T & val) {
-				auto p = _GStorage<ID_Type, T>::GetInstance()->GetP(uniqueID);
+				auto p = Wrapper<ID_Type, T, std::is_convertible<ID_Type, std::string>::value>::GetP(uniqueID);
 				if (p == nullptr)
 					return false;
 
